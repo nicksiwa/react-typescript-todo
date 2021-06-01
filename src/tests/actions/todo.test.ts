bd723e25cfcb40a79877b2db563a980d8510a490
Siwa Khongsuphap <siwa@hospital-os.com>
2021-06-01 22:52:34 +0700

    edit action test

--- src/tests/actions/todo.test.ts
+++ src/tests/actions/todo.test.ts
@@ -1,10 +1,10 @@
 import configureStore from "redux-mock-store";
 import thunk from "redux-thunk";
+import { rest } from "msw";
+import { server } from "../../mocks/server";
 import { TodoAction } from "../../actions";
 import { TodoActionType } from "../../constants/actionTypes";
-import service from "../../services/config";
 
-const mockService = (httpMethod: any) => jest.spyOn(service, httpMethod);
 const middleware: any = [thunk];
 const mockStore = configureStore(middleware);
 
@@ -44,8 +44,6 @@ test("should craete an action start fetch of todos and another action to mark th
     },
   ];
 
-  mockService("get").mockResolvedValueOnce({ data: mockData });
-
   store.dispatch(TodoAction.getTodos() as any);
   store.subscribe(() => {
     expect(store.getActions()).toEqual(expectedAction);
@@ -64,7 +62,11 @@ test("should create an action start fetch of todos and another action to mark th
     },
   ];
 
-  mockService("get").mockRejectedValueOnce(new Error("This error for testing"));
+  server.use(
+    rest.get("/todos", (req, res, ctx) => {
+      return res(ctx.status(500));
+    })
+  );
 
   store.dispatch(TodoAction.getTodos() as any);
   store.subscribe(() => {
@@ -112,7 +114,25 @@ test("should create an action start add of todo and another action to mark the s
     },
   ];
 
-  mockService("post").mockResolvedValueOnce({ data: mockData });
+  store.dispatch(TodoAction.addTodo("demo") as any);
+  store.subscribe(() => {
+    expect(store.getActions()).toEqual(expectedAction);
+    done();
+  });
+});
+
+test("should create an action start add of todo and another action to mark the fail add", (done) => {
+  const store = mockStore({});
+  const expectedAction = [
+    { type: TodoActionType.ADD_TODO_PENDING },
+    { type: TodoActionType.ADD_TODO_FAIL },
+  ];
+
+  server.use(
+    rest.post("/todos", (req, res, ctx) => {
+      return res(ctx.status(500));
+    })
+  );
 
   store.dispatch(TodoAction.addTodo("demo") as any);
   store.subscribe(() => {
@@ -121,18 +141,130 @@ test("should create an action start add of todo and another action to mark the s
   });
 });
 
-test("should create an action start add of todo and another action to mark the failure add", (done) => {
+test("should create an action for pending toggle todo", () => {
+  const expectedAction = {
+    type: TodoActionType.TOGGLE_TODO_PENDING,
+  };
+
+  expect(TodoAction.toggleTodoPending()).toEqual(expectedAction);
+});
+
+test("should create an action for successful toggle todo", () => {
+  const mockTodo: Todo = { id: "1", title: "demo", isCompleted: true };
+  const expectedAction = {
+    type: TodoActionType.TOGGLE_TODO_SUCCESS,
+    payload: mockTodo,
+  };
+
+  expect(TodoAction.toggleTodoSuccess(mockTodo)).toEqual(expectedAction);
+});
+
+test("should create an action for failure toggle todo", () => {
+  const expectedAction = {
+    type: TodoActionType.TOGGLE_TODO_FAIL,
+  };
+
+  expect(TodoAction.toggleTodoFail()).toEqual(expectedAction);
+});
+
+test("should create an action start toggle of todo and another action to mark the success toggle", (done) => {
   const store = mockStore({});
+  const mockData: Todo = { id: "1", title: "demo", isCompleted: true };
   const expectedAction = [
-    { type: TodoActionType.ADD_TODO_PENDING },
-    { type: TodoActionType.ADD_TODO_FAIL },
+    {
+      type: TodoActionType.TOGGLE_TODO_PENDING,
+    },
+    {
+      type: TodoActionType.TOGGLE_TODO_SUCCESS,
+      payload: mockData,
+    },
   ];
 
-  mockService("post").mockRejectedValueOnce(
-    new Error("This error for testing")
+  store.dispatch(TodoAction.toggleTodo("1", false) as any);
+  store.subscribe(() => {
+    expect(store.getActions()).toEqual(expectedAction);
+    done();
+  });
+});
+
+test("should create an action start toggle of todo and another action to mark the fail toggle", (done) => {
+  const store = mockStore({});
+  const expectedAction = [
+    { type: TodoActionType.TOGGLE_TODO_PENDING },
+    { type: TodoActionType.TOGGLE_TODO_FAIL },
+  ];
+
+  server.use(
+    rest.patch("/todos/:id", (req, res, ctx) => {
+      return res(ctx.status(500));
+    })
   );
 
-  store.dispatch(TodoAction.addTodo("demo") as any);
+  store.dispatch(TodoAction.toggleTodo("1", false) as any);
+  store.subscribe(() => {
+    expect(store.getActions()).toEqual(expectedAction);
+    done();
+  });
+});
+
+test("should create an action for pending delete of todo", () => {
+  const expectedAction = {
+    type: TodoActionType.DELETE_TODO_PENDING,
+  };
+
+  expect(TodoAction.deleteTodoPending()).toEqual(expectedAction);
+});
+
+test("should create an action for successful delete of todo", () => {
+  const expectedAction = {
+    type: TodoActionType.DELETE_TODO_SUCCESS,
+    id: "1",
+  };
+
+  expect(TodoAction.deleteTodoSuccess("1")).toEqual(expectedAction);
+});
+
+test("should create an action for failure delete of todo", () => {
+  const expectedAction = {
+    type: TodoActionType.DELETE_TODO_FAIL,
+  };
+
+  expect(TodoAction.deleteTodoFail()).toEqual(expectedAction);
+});
+
+test("should create an action start delete of todo and another action to mark the success delete", (done) => {
+  const store = mockStore({});
+  const expectedAction = [
+    {
+      type: TodoActionType.DELETE_TODO_PENDING,
+    },
+    {
+      type: TodoActionType.DELETE_TODO_SUCCESS,
+      id: "1",
+    },
+  ];
+
+  store.dispatch(TodoAction.deleteTodo("1") as any);
+  store.subscribe(() => {
+    expect(store.getActions()).toEqual(expectedAction);
+    done();
+  });
+});
+
+test("should create an action start delete of todo and another action to mark the fail delete", (done) => {
+  const store = mockStore({});
+  const expectedAction = [
+    { type: TodoActionType.DELETE_TODO_PENDING },
+    { type: TodoActionType.DELETE_TODO_FAIL },
+  ];
+
+  server.use(
+    rest.delete("/todos/:id", (req, res, ctx) => {
+      return res(ctx.status(500));
+    })
+  );
+
+  store.dispatch(TodoAction.deleteTodo("1") as any);
   store.subscribe(() => {
     expect(store.getActions()).toEqual(expectedAction);
     done();
